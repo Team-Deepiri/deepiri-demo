cd9a054165f8c9d3ebbb8c93e09cf8874db9ecf0
Sean San <seanksan@gmail.com>
2026-04-21 08:58:47 -0700

    test: Bad JS that causes CodeQL to fail

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
-const secret_key = '12345-ABCDE'; 
+// Hardcoded secret (CodeQL can flag this)
+const secret_key = '12345-ABCDE';
+
+// Dangerous eval
 function evaluateInput(input) {
-  return eval(input); 
+  return eval(input);
 }
-git 
+
+// Command injection (Node.js)
+const { exec } = require('child_process');
+function runCommand(userInput) {
+  exec("ls " + userInput, (err, stdout, stderr) => {
+    console.log(stdout);
+  });
+}
+
+// Insecure HTTP (not HTTPS)
+const http = require('http');
+function fetchData() {
+  http.get('http://example.com', (res) => {
+    console.log(res.statusCode);
+  });
+}
+
+// SQL injection example
+function getUser(db, username) {
+  const query = "SELECT * FROM users WHERE name = '" + username + "'";
+  return db.query(query);
+}
+
+// Prototype pollution risk
+function merge(target, source) {
+  for (let key in source) {
+    target[key] = source[key];
+  }
+  return target;
+}
+
+// Weak randomness
+function generateToken() {
+  return Math.random().toString(36).substring(2);
+}
+
+// Hardcoded credentials
+const password = "super_secret_password";
+
+// Path traversal
+const fs = require('fs');
+function readFile(userPath) {
+  return fs.readFileSync("/app/data/" + userPath, "utf8");
+}
